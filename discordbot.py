e0e4105f5daca7f5c0e06ea97b54ef79144cc843
tanukinerineri <58840365+tanukinerineri@users.noreply.github.com>
2019-12-14 16:39:55 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,34 +1,27 @@
-from discord.ext import commands
-import os
-import traceback
+# インストールした discord.py を読み込む
+import discord
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = 'THi5IsDuMMyaCCesSTOK3nQ4.Cl2FMQ.ThIsi5DUMMyAcc3s5ToKen7kKWs'
 
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-    
-@bot.command()
-async def 進捗状況確認(ctx):
-    CHANNEL_ID=654935120641261590
-    await ctx.send('はあ')
-    
-@bot.command()
+# メッセージ受信時に動作する処理
+@client.event
 async def on_message(message):
-    # メンバーのリストを取得して表示
-    if message.content == '/members':
-        print(message.guild.members)
-    # 役職のリストを取得して表示
-    if message.content == '/roles':
-        print(message.guild.roles)    
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
-bot.run(token)
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
